b5e82a6ec35fb2163fcf38165017db4861419c54
Pablo Espinosa Gomez <pespinos@student.42malaga.com>
2023-12-06 15:42:52 +0100

    CONTROL PRESENCIA CARACTER JUGADOR

--- src/120errorMessages.c
+++ src/120errorMessages.c
@@ -9,5 +9,7 @@ void	ft_error_messages(int message)
 		printf("SE HAN ENCONTRADO CARACTERES INVALIDOS EN EL MAPA\n\n");
 	else if (message == 3)
 		printf("MUROS NO CERRADOS\n\n");
+	else if (message == 4)
+		printf("NO HAY JUGADOR\n\n");
 	exit(1);
 }
