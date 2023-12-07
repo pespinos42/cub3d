4ab0b416b91eb7c5eb3aa4dd223083ffa0dd7f5c
rdelicad <rdelicad@student.42malaga.com>
2023-12-07 20:22:57 +0100

    creando cielo y tierra

--- src/main_game.c
+++ src/main_game.c
@@ -1,32 +1,5 @@
 #include "../include/cub3d.h"
 
-/* static void color_background(mlx_image_t* img)
-{
-	memset(img->pixels, 100, img->width * img->height * BPP);
-}
-
-static void draw_player(t_data *d)
-{
-	int pixel_size;
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	pixel_size = 10; // Tamaño del "píxel" grande
-	while (i < pixel_size)
-	{
-		j = i + 1;
-		while (j < pixel_size)
-		{
-			mlx_put_pixel(d->img, d->x_position_player + i, d->y_position_player + j, 0xFF0000FF);
-			j++;
-		}
-		i++;
-	}
-} */
-
-
 void map_color_background(t_map *map)
 {
 	uint32_t x;
@@ -62,10 +35,8 @@ int32_t	ft_main_game(t_map *map)
 	if (!map->img)
 		ft_error_messages(5);
 	map_color_background(map);
-	//mlx_image_to_window(map->mlx, map->img, 0, 0);
 	mlx_set_window_title(map->mlx, map->title);
-	/* color_background(d->img);
-	draw_player(d); */
+	/* draw_player(d); */
 	mlx_loop(map->mlx);
 	mlx_terminate(map->mlx);
 	return (EXIT_SUCCESS);
