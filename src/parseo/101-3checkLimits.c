7fced552cbcfd3608361c69e71958646cc636dc6
Pablo Espinosa Gomez <pespinos@student.42malaga.com>
2024-01-17 16:45:28 +0100

    ELIMINADOS MENSAJES FLOOD FILL

--- src/parseo/101-3checkLimits.c
+++ src/parseo/101-3checkLimits.c
@@ -6,10 +6,6 @@ void	ft_check_limits(t_data *d)
 	ft_duplicate_map(d);
 	ft_locate_player(d);
 	ft_flood_fill(d->x_position_player, d->y_position_player, d);
-	printf("--PRIMER FLOOD_FILL--\n");
-	ft_print_matrix(d->map_flood_fill, d);
 	ft_fill_islands(d);
-	printf("--FLOOD_FILL DESPUES DE RELLENAR LAS ISLAS--\n");
-	ft_print_matrix(d->map_flood_fill, d);
 	ft_compare_maps(d);
 }
